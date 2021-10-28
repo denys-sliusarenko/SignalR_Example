63a15252920c0d48c648eb70db5146e044fbe8a2
Denys Sliusarenko <Denys.Sliusarenko@bd.com>
2021-10-28 13:53:47 +0300

    hub context, connect and disconect client

--- SignalR_Example/Hubs/ChatHub.cs
+++ SignalR_Example/Hubs/ChatHub.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace SignalR_Example.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string message, string userName)
+        public async Task Send(string message)
         {
-            await Clients.All.SendAsync("Send", message, userName);
+            await Clients.All.SendAsync("Receive", message, Context.ConnectionId);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} joined chat");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} left chat");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
